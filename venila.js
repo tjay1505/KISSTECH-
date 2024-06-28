ccaf6926f6fe0ed98cc10995accfe569da116ebc
tjay <jaikumartjay@gmail.com>
2024-06-28 15:51:21 +0530

    v-3 28/6 anime

--- venila.js
+++ venila.js
@@ -180,3 +180,33 @@ function openNav() {
     function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     }
+
+    gsap.registerPlugin(ScrollTrigger)
+
+  //   gsap.from("#h12",{
+  //     scrollTrigger:{
+  //         trigger:"#h12",
+  //         scrub:1,
+  //     },
+  //     Opacity:0,
+  //     yPercent:100,
+  //     duration:0.2
+  // })
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
